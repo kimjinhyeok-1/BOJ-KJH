34a7e2677c93730a4c52e101853893aca4586725
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-01-13 16:33:52 +0900

    [Gold V] Title: 회의실 배정, Time: 232 ms, Memory: 54216 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = []
+for _ in range(n):
+    st, end = map(int, input().split())
+    d.append((st, end))
+
+d.sort(key= lambda x: (x[1], x[0]))
+
+count = 0
+last_end = 0
+for a, b in d:
+    if a >= last_end:
+        last_end = b
+        count += 1
+print(count)
