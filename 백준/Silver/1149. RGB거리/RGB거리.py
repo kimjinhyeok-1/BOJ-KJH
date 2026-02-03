be98d6ec6da1bd55f5fe51a49ddabe0ae2dbec2d
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-02-03 15:38:26 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,11 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [[0,0,0]]
+r, g, b = map(int, input().split())
+prev = [r, g, b]
 
-for i in range(1,n+1):
-    dp.append(list(map(int, input().split())))
-    dp[i][0] = dp[i][0] + min(dp[i-1][1], dp[i-1][2])
-    dp[i][1] = dp[i][1] + min(dp[i-1][0], dp[i-1][2])
-    dp[i][2] = dp[i][2] + min(dp[i-1][0], dp[i-1][1])
-print(min(dp[n]))
+for _ in range(n - 1):
+    r, g, b = map(int, input().split())
+    cur = [
+        r + min(prev[1], prev[2]),
+        g + min(prev[0], prev[2]),
+        b + min(prev[0], prev[1])
+    ]
+    prev = cur
+
+print(min(prev))
