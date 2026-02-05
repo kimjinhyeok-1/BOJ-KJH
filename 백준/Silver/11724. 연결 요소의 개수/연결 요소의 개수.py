0d408cc99da20355375f0808c087905505f620dc
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-02-05 12:58:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 368 ms, Memory: 171860 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,26 +1,30 @@
-import sys
+import sys 
+from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
 
-n,m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+n,m = map(int,input().split())
+stack = [[] for _ in range(n+1)]
+for _ in range(1, m+1):
+    a, b = map(int, input().split())
+    stack[a].append(b)
+    stack[b].append(a)
+for i in range(1,n+1):
+    stack[i].sort()
+
 visited = [False] * (n+1)
-
-for _ in range(m):
-    a,b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-def dfs(v):
-    visited[v] = True
-    for nxt in graph[v]:
-        if not visited[nxt]:
-            dfs(nxt)
-
 count = 0
-for v in range(1, n+1):
-    if not visited[v]:
-        dfs(v)
-        count += 1
+q = deque()
 
+    
+for i in range(1,n+1):
+    if visited[i] == False:
+        q.append(i)
+        visited[i] = True
+        while q:
+            v = q.popleft()
+            for nx in stack[v]:
+                if visited[nx] == False:
+                    visited[nx] = True
+                    q.append(nx)
+        count += 1
 print(count)
