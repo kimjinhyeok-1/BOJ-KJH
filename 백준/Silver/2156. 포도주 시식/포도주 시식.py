868c41c6769ad7717ef12e032090d4f060daf129
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-02-06 15:16:04 +0900

    [Silver I] Title: 포도주 시식, Time: 104 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -3,13 +3,24 @@ input = sys.stdin.readline
 INF = 10**18
 n = int(input())
 
-gr = [0] + [int(input()) for _ in range(n)]
-dp = [[0,0,0] for _ in range(n+1)]
-dp[0][0] = 0
-dp[0][1] = -INF
-dp[0][2] = -INF
-for i in range(1, n+1):
-    dp[i][0] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2] )
-    dp[i][1] = dp[i-1][0] + gr[i]
-    dp[i][2] = dp[i-1][1] + gr[i]
-print(max(dp[n]))
+# gr = [0] + [int(input()) for _ in range(n)]
+# dp = [[0,0,0] for _ in range(n+1)]
+# dp[0][0] = 0
+# dp[0][1] = -INF
+# dp[0][2] = -INF
+# for i in range(1, n+1):
+#     dp[i][0] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2] )
+#     dp[i][1] = dp[i-1][0] + gr[i]
+#     dp[i][2] = dp[i-1][1] + gr[i]
+# print(max(dp[n]))
+
+ans =  [0, -INF, -INF]
+for _ in range(n):
+    w = int(input())
+    gr = [
+        max(ans[0], ans[1], ans[2]),
+        ans[0] + w,
+        ans[1] + w
+    ]
+    ans = gr
+print(max(ans))
