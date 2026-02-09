f6f727dec32e2cb62e5cb0995a385e8b6a37e1cf
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-02-09 12:52:06 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 296 ms, Memory: 153260 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -2,11 +2,18 @@ import sys
 input = sys.stdin.readline
 
 word = input().strip()
+l = len(word)
+ans = [[0] * (l+1) for _ in range(26)]
+for i in range(1,l+1):
+    for j in range(26):
+        ans[j][i] = ans[j][i-1]
+    ans[ord(word[i-1]) - ord('a')][i] += 1  
+
+
+
 q = int(input())
 for i in range(q):
     a, i, j = map(str, input().split())
-    count = 0
-    for k in range(int(i), int(j)+1):
-        if word[k] == a:
-            count += 1
-    print(count)
+    i = int(i)
+    j = int(j)
+    print(ans[ord(a) - ord('a')][j+1] - ans[ord(a) - ord('a')][i])
