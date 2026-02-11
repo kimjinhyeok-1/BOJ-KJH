de33711dc778fe2651098ab5efee57474e30ce93
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-02-11 13:08:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 528 ms, Memory: 267840 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,17 +1,19 @@
 import sys
+from collections import defaultdict
 input = sys.stdin.readline
 
 n = int(input())
-card = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 m = int(input())
-find = list(map(int, input().split()))
+s = list(map(int, input().split()))
 
-count = {}
-for x in card:
-    count[x] = count.get(x, 0) + 1
+ans = defaultdict(int)
+
+for x in nums:
+    ans[x] += 1
+
 out = []
 
-for x in find:
-    out.append(str(count.get(x, 0)))
-
+for x in s:
+    out.append(str(ans[x]))
 print(" ".join(out))
