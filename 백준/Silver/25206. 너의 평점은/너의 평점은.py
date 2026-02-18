6ae9cc5a866b055c2625bc22a0d104118f64d26e
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-02-18 21:34:22 +0900

    [Silver V] Title: 너의 평점은, Time: 76 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+mp = {
+    "A+": 4.5, "A0": 4.0,
+    "B+": 3.5, "B0": 3.0,
+    "C+": 2.5, "C0": 2.0,
+    "D+": 1.5, "D0": 1.0,
+    "F": 0.0
+}
+
+tot = 0.0
+sumv = 0.0
+
+for _ in range(20):
+    _, s, g = input().split()
+    if g == "P":
+        continue
+    s = float(s)
+    tot += s
+    sumv += s * mp[g]
+
+print(sumv / tot)
