36d56afdd6526ce477d43a1cc43e1582e393b057
kimjinhyeok-1 <jinxxang123@gmail.com>
2026-03-04 14:44:57 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1332 ms, Memory: 287720 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,27 +1,30 @@
-import sys 
+import sys
 from collections import deque
 input = sys.stdin.readline
 
-N,M = map(int,input().split())
-grid = [list(map(int,input().strip())) for _ in range(N)]
-dist = [[[-1] * 2 for _ in range(M)] for _ in range(N)]
-d = [(1,0),(-1,0),(0,1),(0,-1)]
+n,m = map(int, input().split())
+a = [list(map(int, input().strip())) for _ in range(n)]
+dist = [[[-1] * 2 for _ in range(m)] for _ in range(n)]
 
 q = deque([(0,0,0)])
+
+d = [(0,1),(0,-1),(1,0),(-1,0)]
+
 dist[0][0][0] = 1
 
 while q:
-    x,y,z = q.popleft()
-    if x == M-1 and y == N-1:
-        print(dist[y][x][z])
-        exit()
-    for dx, dy in d:
-        nx, ny = x+dx, y+dy
-        if 0<=nx<M and 0<=ny<N and grid[ny][nx] == 0 and dist[ny][nx][z] == -1:
-            dist[ny][nx][z] = dist[y][x][z] + 1
-            q.append((nx,ny,z))
-        elif 0<=nx<M and 0<=ny<N and z == 0 and grid[ny][nx] == 1 and dist[ny][nx][1] == -1:
-            dist[ny][nx][1] = dist[y][x][0] + 1
-            q.append((nx,ny,1))
-
+    r, c, des = q.popleft()
+    if r == n-1 and c == m-1:
+        print(dist[n-1][m-1][des])
+        exit(0)
+    for dr, dc in d:
+        nr = r + dr
+        nc = c + dc
+        if 0<= nr < n and 0<= nc < m:
+            if a[nr][nc] == 0 and dist[nr][nc][des] == -1:
+                dist[nr][nc][des] = dist[r][c][des] + 1
+                q.append((nr,nc,des))
+            elif des == 0 and a[nr][nc] == 1 and dist[nr][nc][1] == -1:
+                dist[nr][nc][1] = dist[r][c][0] + 1
+                q.append((nr,nc,1))
 print(-1)
