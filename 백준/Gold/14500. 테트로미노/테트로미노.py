5683b4ce15d68535c6e7b6029f07aed7c798a06c
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-18 14:40:51 +0900

    [Gold IV] Title: 테트로미노, Time: 796 ms, Memory: 117020 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,42 +1,50 @@
 import sys
 input = sys.stdin.readline
+
 n,m = map(int, input().split())
 
 ans = 0
-ds = [[(0,1),(0,2),(0,3)], [(1,0),(2,0),(3,0)], # ㅡ 모양 ㅣ 모양
-      
-     [(1,0),(0,1),(1,1)], # ㅁ 모양
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+d = [(1,0),(-1,0),(0,1),(0,-1)]
 
-     [(1,0),(2,0),(2,1)], [(0,1),(0,2),(-1,2)], # L 모양 (회전 포함 8개)
-     [(0,1),(1,1),(2,1)], [(1,0),(1,1),(1,2)],
-
-     [(0,1),(-1,1),(-2,1)], [(0,1),(0,2),(1,2)],
-     [(0,1),(1,0),(2,0)], [(1,0),(0,1),(0,2)],
-
-     [(0,-1),(0,1),(1,0)], [(0,-1),(1,0),(-1,0)], # ㅗ 모양 (회전 포함 4개)
-     [(0,-1),(0,1),(-1,0)], [(0,1),(1,0),(-1,0)],
-
-     [(1,0),(1,1),(2,1)], [(0,1),(-1,1),(-1,2)], # 번개모양 (회전 포함 4개)
-     [(1,0),(0,1),(-1,1)], [(0,1),(1,1),(1,2)] 
-     ]
-
-grid  = [list(map(int, input().split())) for _ in range(n)]
-
-def bfs(r,c,d):
-    temp = grid[r][c]
-
+def dfs(r,c,depth,v):
+    global ans
+    if depth == 4:
+        ans = max(ans, v)
+        return
     for dr, dc in d:
-        nr,nc = r + dr, c + dc
-        if nr<0 or nr>=n or nc<0 or nc>=m:
-            return 0
-        else:
-            temp += grid[nr][nc]
-    return temp
+        nr, nc = r+dr,c+dc
+        if 0<=nr<n and 0<=nc<m and not visited[nr][nc]:
+            visited[nr][nc] = True
+            dfs(nr,nc,(depth +1), (v+grid[nr][nc]))
+            visited[nr][nc] = False
+        
+def other(r,c,v):
+    arr = []
+    for dr,dc in d:
+        nr,nc = r+dr,c+dc
+        if 0<=nr<n and 0<=nc<m:
+            arr.append(grid[nr][nc])
+    l = len(arr)
+    if l < 3:
+        return 0
+    elif l == 3:
+        v = v + sum(arr)
+    else:
+        v = v + sum(arr) - min(arr)
+        
+    return v
+
+
 
 
 for r in range(n):
     for c in range(m):
-        for d in ds:
-            ans = max(ans, bfs(r,c,d))
+        visited[r][c] = True
+        dfs(r,c,1,grid[r][c])
+        visited[r][c] = False
+        temp2 = other(r,c,grid[r][c])
+        ans = max(ans,temp2)
 
 print(ans)
