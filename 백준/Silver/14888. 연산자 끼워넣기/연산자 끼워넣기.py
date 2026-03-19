e89909b8a94358acc411fbfebcb747c2a998388e
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-19 15:45:51 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 176 ms, Memory: 112112 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -3,37 +3,31 @@ from itertools import permutations
 input = sys.stdin.readline
 MAX = 1000000000
 MIN = -MAX
-
+ma = MIN
+mi = MAX
 n = int(input())
 arr = list(map(int, input().split()))
 plus, minus, time, divide = map(int,input().split())
 
-ops = []
-ops += ['+'] * plus
-ops += ['-'] * minus
-ops += ['*'] * time
-ops += ['/'] * divide
+def dfs(idx, result, plus, minus, time, divide):
+    global ma,mi
+    if idx == n:
+        ma = max(ma,result)
+        mi = min(mi,result)
+        return
+    if plus>0:
+        dfs(idx+1, result + arr[idx], plus -1, minus,time,divide)
+    if minus>0:
+        dfs(idx+1, result - arr[idx], plus, minus-1,time,divide)
+    if time>0:
+        dfs(idx+1, result * arr[idx], plus, minus,time-1,divide)
+    if divide>0:
+        if result<0:
+            dfs(idx+1, -((-result) // arr[idx]), plus, minus,time,divide-1)
+        else:
+            dfs(idx+1, result // arr[idx], plus, minus, time, divide - 1)
 
-cases = set(permutations(ops))
-
-ma = MIN
-mi = MAX
-for case in cases:
-    result = arr[0]
-    for i in range(len(case)):
-        if case[i] == '+':
-            result += arr[i+1]
-        elif case[i] == '-':
-            result -= arr[i+1]
-        elif case[i] == '*':
-            result *= arr[i+1]
-        elif case[i] == '/':
-            if result < 0:
-                result = -((-result) // arr[i+1])
-            elif result>=0:
-                result = result // arr[i+1]
-    ma = max(ma,result)
-    mi = min(mi,result)
+dfs(1, arr[0], plus, minus, time, divide)
 
 print(ma)
 print(mi)
