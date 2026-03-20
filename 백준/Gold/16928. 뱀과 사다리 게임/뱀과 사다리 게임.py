dd06ed5add56d19cfae8c8ed9b16e542f138a794
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-20 19:25:48 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 116 ms, Memory: 110068 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+move = [0] * 101
+
+for _ in range(n+m):
+    a,b = map(int, input().split())
+    move[a] = b
+
+dist = [-1] * 101
+q = deque([1])
+dist[1] = 0
+
+while q:
+    x = q.popleft()
+    for dx in range(1,7):
+        nx = x + dx
+        if nx > 100:
+            break
+        if move[nx] != 0:
+            nx = move[nx]
+        if dist[nx] == -1:
+            dist[nx] = dist[x] + 1
+            q.append(nx)
+
+print(dist[100])
