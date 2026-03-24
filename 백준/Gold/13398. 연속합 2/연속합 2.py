e7a305ac782408a97be44af5ee4743bdcd60f4f4
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-24 23:39:17 +0900

    [Gold IV] Title: 연속합 2, Time: 128 ms, Memory: 131884 KB -BaekjoonHub

---
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [[0] * 2 for _ in range(n)]
+dp[0][1] = dp[0][0] = arr[0]
+
+ans = arr[0]
+for i in range(1, n):
+    dp[i][0] = max(arr[i], dp[i-1][0] + arr[i])
+    dp[i][1] = max(dp[i-1][0], dp[i-1][1] + arr[i])
+    ans = max(ans, dp[i][0], dp[i][1])
+
+print(ans)
