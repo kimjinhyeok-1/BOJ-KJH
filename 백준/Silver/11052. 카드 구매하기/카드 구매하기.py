56238eb582a6d9d94c4d9353cac5f2ccdc0b1b80
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-24 22:20:10 +0900

    [Silver I] Title: 카드 구매하기, Time: 120 ms, Memory: 111312 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,9 +1,13 @@
-import sys 
+import sys
+from collections import deque
 input = sys.stdin.readline
+
 n = int(input())
-prices = list(map(int, input().split()))
+MOD = 10007
+prices = [0] + list(map(int, input().split()))
 dp = [0] * (n+1)
-for i in range(0, n):
-    for j in range(i+1, n+1):
-        dp[j] = max(dp[j], dp[j-i-1] + prices[i] * 1)
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j] + prices[j])
 print(dp[n])
