9a0e21ef1c001528ae9c2d2ca08b54eea465b3b6
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-27 14:29:52 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 117336 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -4,7 +4,10 @@ input = sys.stdin.readline
 
 n = int(input())
 q = deque(range(1,n+1))
-while len(q) > 1:
-    q.popleft()
-    q.append(q.popleft())
-print(q[0])
+while True:
+    k = q.popleft()
+    if q:
+        q.append(q.popleft())
+    else:
+        break
+print(k)
