7048896556ff7325b4934a445ab2cc25c429effa
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-28 11:19:22 +0900

    [Silver IV] Title: 듣보잡, Time: 136 ms, Memory: 115824 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,16 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-heard = {input().strip() for _ in range(n)}
-
-ans = []
+n,m = map(int, input().split())
+list_d = {input().strip() for _ in range(n)}
+list_b = []
 
 for _ in range(m):
     name = input().strip()
-    if name in heard:
-        ans.append(name)
-        
-ans.sort()
-print(len(ans))
-print("\n".join(ans))
+    if name in list_d:
+        list_b.append(name)
+
+list_b.sort()
+print(len(list_b))
+print("\n".join(list_b))
