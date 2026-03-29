1cbc122a727c936cee3a77222c2611abbf336ccf
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-29 15:24:54 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,43 +1,35 @@
-import sys
-input = sys.stdin.readline
-
-passwords = []
-m = {'a','e','i','o','u'}
-while True:
-    x = input().strip()
-    if x == 'end':
-        break
-    passwords.append(x)
-
-for password in passwords:
-    ok = True
-    has_m = False
-    m_count = 0
-    j_count = 0
+def check(password):
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    has_vowel = False
+    v_cnt = 0
+    c_cnt = 0
     prev = ''
 
     for ch in password:
-        # 모음인 경우에는 모음 가지기 성공 + 모음 카운트 하나 올리기 + 자음 카운트 0으로 초기화
-        # 자음인 경우에는 반대로 
-        if ch in m:
-            has_m = True
-            m_count += 1
-            j_count = 0
+        if ch in vowels:
+            has_vowel = True
+            v_cnt += 1
+            c_cnt = 0
         else:
-            j_count += 1
-            m_count = 0
+            c_cnt += 1
+            v_cnt = 0
 
-        # 모음이나 자음 카운트가 3이상인 경우 ok는 False
-        if j_count >= 3 or m_count >= 3:
-            ok = False
-        
-        # 같은 글자가 왔는데 해당 글자가 o, e가 아닌 경우 ok는 False
-        if prev == ch and ch not in ('o','e'):
-            ok = False
+        if v_cnt >= 3 or c_cnt >= 3:
+            return False
 
-        # 이전 글자 초기화
+        if prev == ch and ch not in ('e', 'o'):
+            return False
+
         prev = ch
-    if has_m and ok:
-        print('<' + password + '>' + ' is acceptable.')
+
+    return has_vowel
+
+while True:
+    password = input().strip()
+    if password == 'end':
+        break
+
+    if check(password):
+        print(f'<{password}> is acceptable.')
     else:
-        print('<' + password + '>' + ' is not acceptable.')
+        print(f'<{password}> is not acceptable.')
