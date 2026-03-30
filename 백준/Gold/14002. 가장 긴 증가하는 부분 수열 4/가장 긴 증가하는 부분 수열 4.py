c9110f57f528364dd2823228fb51f6dc9bd379c7
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-30 16:42:46 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,21 +1,30 @@
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
-
+INF = 10**18
 n = int(input())
-lst = list(map(int, input().split()))
-dp =  [1] * n
-parent = [-1] * n
-for i in range(n):
-    for j in range(i):
-        if lst[j] < lst[i]:
-            if dp[j] + 1 > dp[i]:
-                dp[i] = dp[j] + 1
-                parent[i] = j
-print(max(dp))
-idx = dp.index(max(dp))
-ans = []
-for i in range(max(dp)):
-    ans.append(lst[idx])
-    idx = parent[idx]
-ans.reverse()
-print(" ".join(map(str,ans)))
+nums = list(map(int, input().split()))
+out = []
+dp = []
+for x in nums:
+    if len(out) == 0 or out[-1] < x:
+        out.append(x)
+        k = len(out) - 1
+        dp.append([x,k])
+    else:
+        idx = bisect_left(out, x)
+        out[idx] = x
+        dp.append([x,idx])
+l = len(out)
+dp.reverse()
+
+result = []
+i = l-1
+for j in range(len(dp)):
+    if dp[j][1] == i:
+        result.append(dp[j][0])
+      
+        i -= 1
+print(l)
+result.reverse()
+print(" ".join(map(str, result)))
