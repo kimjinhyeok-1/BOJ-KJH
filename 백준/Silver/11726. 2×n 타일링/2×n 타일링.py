00beba15922137a0610e1a2fdda724f0e21f0665
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-31 15:37:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,16 +1,17 @@
 import sys
 input = sys.stdin.readline
-MOD = 10007
+
 n = int(input())
+MOD = 10007
+
 dp = [0] * (n+1)
+
 if n == 1:
     print(1)
-    sys.exit(0)
-if n == 2:
-    print(2)
-    sys.exit(0)
-dp[1], dp[2] = 1,2
-for i in range(3,n+1):
-    dp[i] = dp[i-1] + dp[i-2]
-
-print(dp[n] % MOD)
+    sys.exit()
+elif n>=2:
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3,n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD
+    print(dp[n])
