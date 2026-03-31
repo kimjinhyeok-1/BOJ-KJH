c91587b201eecfefdfee227feb490a7624192f4b
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-31 22:25:18 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 120 ms, Memory: 110064 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,27 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+s = input().strip()
+count = Counter(s)
+
+count_p = 0
+mid = ''
+for ch, c in count.items():
+    if c % 2 == 1:
+        mid = ch
+        count_p += 1
+
+if count_p > 1:
+    print('I\'m Sorry Hansoo')
+    sys.exit()
+
+left = ''
+for ch in sorted(count):
+    for _ in range(count[ch] // 2):
+        left += ch
+
+right = "".join(reversed(left))
+ans = left + mid + right
+
+print(ans)
