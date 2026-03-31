e2d47b6a0094b41b6f9d6618e8d2fbb725522d2f
Jinhyeok Kim <jinxxang123@gmail.com>
2026-03-31 14:59:48 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 300 ms, Memory: 128876 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,13 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
-tb = [list(map(int, input().split())) for _ in range(n)]
-ans = [[0] * (n+1) for _ in range(n+1)]
+n ,m = map(int, input().split())
+grid = [[0] * (n+1)]
+for _ in range(n):
+    grid.append([0] + list(map(int, input().split())))
+
+p = [[0] * (n+1) for _ in range(n+1)]
+
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        ans[i][j] = ans[i][j-1]+ ans[i-1][j] - ans[i-1][j-1] + tb[i-1][j-1]
+    for j in range(1, n+1):
+        p[i][j] = p[i-1][j] + p[i][j-1] - p[i-1][j-1] + grid[i][j]
+
 for _ in range(m):
-    x1,y1,x2,y2 = map(int, input().split())
-    k = ans[x2][y2] - ans[x1-1][y2] - ans[x2][y1-1] + ans[x1-1][y1-1]
-    print(k)
+    r1,c1,r2,c2 = map(int, input().split())
+    print(p[r2][c2] - p[r2][c1-1] - p[r1-1][c2] + p[r1-1][c1-1])
