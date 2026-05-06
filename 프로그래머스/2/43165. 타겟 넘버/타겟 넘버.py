7892db27483b0f0c6acb93f075f91ea30f48804c
Jinhyeok Kim <jinxxang123@gmail.com>
2026-05-06 20:40:31 +0900

    [level 2] Title: 타겟 넘버, Time: 366.11 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,17 @@
+# 모든 경우의 수를 다 보고 타깃에 맞는 갯수 구하는거니까 완전탐색
 def solution(numbers, target):
-    answer = [0]
     n = len(numbers)
+    def dfs(cnt, val):
+        if cnt == n:
+            if val == target:
+                return 1
+            else:
+                return 0
+        else:
+            answer = 0
+            answer += dfs(cnt+1, val+numbers[cnt])
+            answer += dfs(cnt+1, val-numbers[cnt])
+        return answer
     
-    def dfs(idx, total):
-        if idx == n:
-            if total == target:
-                answer[0] += 1
-            return
-        dfs(idx+1, total + numbers[idx])
-        dfs(idx+1, total - numbers[idx])
-    dfs(0,0)    
-        
-    return answer[0]
+    answer = dfs(0, 0)
+    return answer
